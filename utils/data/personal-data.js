cf48f7be3caf68ca6a02437f4baea2f2b1eb810e
Jin Ustad (Gul Mohammad) <96934693+JinUstad@users.noreply.github.com>
2025-04-09 12:45:21 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -12,5 +12,5 @@ export const personalData = {
   twitter: '',
   stackOverflow: '',
   leetcode: "",
-  resume: "https://drive.google.com/file/d/1vpcxelqxiObJuaTI-lf0uyeme6FcVpci/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1hqnLUXcUkDlu3JTxA80kgii-sfy9jfKp/view?usp=sharing"
 }
