26b6b059f3663aac994aeb3fb2f8db0d60697b35
Jin Ustad (Gul Mohammad) <96934693+JinUstad@users.noreply.github.com>
2025-04-09 12:28:30 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
+    title: 'Software Engineer',
+    company: "Krapton IT Consultancy",
+    duration: "(January 2025 - Present)"
+  },
+  {
+    id: 2,
     title: 'Software Engineer (UI/UX Designer)',
     company: "Chetu Inc.",
     duration: "(July 2023 - Dec 2024)"
   },
   {
-    id: 2,
+    id: 3,
     title: "Web Developer",
     company: "MasterTec",
     duration: "(Jan 2023 - June 2023)"
-  },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something everyday.",
-    duration: "(Jan 2018 - Present)"
   }
 ]
